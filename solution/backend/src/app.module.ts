aec7302b948755041c46fae2c5e5b0ae9cf3734f
thibault <tititrois73@gmail.com>
2023-10-05 16:27:47 +0800

    add config to load config from the json

--- solution/backend/src/app.module.ts
+++ solution/backend/src/app.module.ts
@@ -3,15 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Route } from "./entities/route.entity";
+import * as fs from "fs";
+
+const CONFIG_FILE_NAME= "millennium-falcon.json";
+interface Config {
+  "autonomy": number,
+  "departure": string,
+  "arrival": string,
+  "routes_db": string
+}
 
 @Module({
   imports: [
-    MikroOrmModule.forRoot({
-      entities: ['./dist/entities'],
-      entitiesTs: ['./src/entities'],
-      dbName: 'database/universe.db',
-      type: 'better-sqlite',
-      port: 5432
+    MikroOrmModule.forRootAsync({
+      useFactory: () => {
+        const config: Config = JSON.parse(fs.readFileSync(CONFIG_FILE_NAME, 'utf8'));
+
+        return {
+          entities: ['./dist/entities'],
+            entitiesTs: ['./src/entities'],
+          dbName: config["routes_db"],
+          type: 'better-sqlite',
+          port: 5432
+        }
+      }
     }),
     MikroOrmModule.forFeature([Route])
   ],
