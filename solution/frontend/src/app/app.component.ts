b25d61dfe7e9fa6c2cba8044b0dbbc012b140d10
thibault <tititrois73@gmail.com>
2023-10-07 16:33:45 +0800

    implement logic in the frontend to display and call backend

--- solution/frontend/src/app/app.component.ts
+++ solution/frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ApiService } from "./api.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  private millenniumFalconData: any; // Store JSON data
+  public probability: number | null = null;
+
+  constructor(private apiService: ApiService) {}
+
+  // Function to handle file selection
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.millenniumFalconData = JSON.parse(e.target?.result as string);
+      };
+      reader.readAsText(file);
+    }
+  }
+
+  // Function to calculate probability
+  calculateProbability() {
+    if (!this.millenniumFalconData) {
+      alert('Please upload a JSON file first.');
+      return;
+    }
+
+    // Send the data to the backend API
+    this.apiService.calculateProbability(this.millenniumFalconData)
+      .subscribe((response) => {
+        this.probability = response;
+      });
+  }
+
+  // Function to get probability message
+  getProbabilityMessage(probability: number): string {
+    if (probability === 100) {
+      return 'The Millennium Falcon can reach Endor without encountering bounty hunters.';
+    } else if (probability > 0) {
+      return 'The Millennium Falcon can reach Endor but might be captured by bounty hunters.';
+    } else {
+      return 'The Millennium Falcon cannot reach Endor before the Death Star annihilates it.';
+    }
+  }
 }
