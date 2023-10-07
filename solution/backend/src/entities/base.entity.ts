b0ca6c01f434ab8d51e378506de9504ad8b237e0
thibault <tititrois73@gmail.com>
2023-10-07 12:02:05 +0800

    remove not needed properties for base model

--- solution/backend/src/entities/base.entity.ts
+++ solution/backend/src/entities/base.entity.ts
@@ -2,13 +2,7 @@ import { PrimaryKey, Property } from '@mikro-orm/core';
 
 export abstract class BaseEntity {
   @PrimaryKey()
-  id!: number;
-
-  @Property()
-  createdAt: Date = new Date();
-
-  @Property({ onUpdate: () => new Date() })
-  updatedAt: Date = new Date();
+  id?: number;
 }
 
 
